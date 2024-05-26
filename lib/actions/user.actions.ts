fb344974b4023fd3716419ebe25b56d2a403210f
karthik <karthiknadar1204@gmail.com>
2024-05-26 11:29:07 +0530

    implement auth page ui

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,33 @@
+'use server';
+
+export const signIn= async({ email, password }: signInProps)=>{
+    try {
+        //mutation/database/fetch
+
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+
+export const signUp= async({ password, ...userData }: SignUpParams)=>{
+    try {
+        //mutation/database/fetch
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+
+export const getLoggedInUser= async()=>{
+    try {
+        //mutation/database/fetch
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
