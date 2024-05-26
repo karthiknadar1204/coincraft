52ce1408155d707cdf82ca0839325150d2b07634
karthik <karthiknadar1204@gmail.com>
2024-05-26 09:31:50 +0530

    implement basic charts on homepage to render static data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            // user={loggedIn?.firstName || 'Guest'}
+            user='Karthik'
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2000}
+          />
+        </header>
+
+        {/* <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      {/* <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      /> */}
+    </section>
   )
 }
 
